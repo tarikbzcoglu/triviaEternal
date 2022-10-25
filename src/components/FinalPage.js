c334f0e243533a90d9b848da6b6b446d3f8a5fca
Tarık <feanorwastaken@gmail.com>
2022-10-25 19:26:01 +0300

    confetti effect added to final page

--- src/components/FinalPage.js
+++ src/components/FinalPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Confetti from 'react-confetti'
 
 export default function FinalPage({
   score,
@@ -10,6 +11,10 @@ export default function FinalPage({
   
   return (
     <div>
+      <Confetti
+       width={window.innerWidth - 10 || 300}
+       height={window.innerHeight -10 || 200}
+      />
       <h1 className = "end-text">Congratulations!, Your score is: {score}</h1>
       <button onClick={handlePlayagain} className = "button">Play Again</button>
       </div>
